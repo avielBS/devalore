43c8d979980b8ae0c131057d7772896711a9704a
AvielBS <aviel2845@gmail.com>
2020-11-24 19:44:41 +0200

    init

---
+++ main.py
@@ -0,0 +1,50 @@
+import json
+import pathlib
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def invalid_page(e):
+    return 'page not exist'
+
+
+def get_path():
+    if app.config['TESTING']:
+        return str(pathlib.Path(__file__).parent) + "/tests/testUsers.json"
+    else:
+        return str(pathlib.Path(__file__).parent) + '/users.json'
+
+
+def read_from_file(path):
+    f = open(path, 'r')
+    data = json.load(f)
+    f.close()
+    return data
+
+
+@app.route('/users')
+def users_path():
+    path = get_path()
+    users = read_from_file(path)
+    for i in users.values():
+        i.pop('id')
+    return users
+
+
+@app.route('/users/<username>')
+def users_username_path(username):
+    path = get_path()
+    users = read_from_file(path)
+    if username not in users.keys():
+        return 'user does not exist in the database', 404
+    d = {username: users[username]}
+    return d
+
+
+if __name__ == '__main__':
+    print(pathlib.Path(__file__).parent)
+    app.run(debug=True)
---
+++ tests/test_main.py
@@ -0,0 +1,46 @@
+from main import app
+import unittest
+import json
+
+
+class FlaskTest(unittest.TestCase):
+
+    def test_first(self):
+        tester = app.test_client(self)
+        app.config['TESTING'] = True
+        res = tester.get('/users')
+        data = res.data.decode('utf8').replace("'", '"')
+        data = json.loads(data)
+
+        # for fail this test
+        # data['duncan_long']['id'] = 1
+
+        expected_output = {"test_user": {"name": "Test User", "favorite_color": "Black"}}
+        self.assertEqual(data, expected_output)
+
+    def test_second(self):
+        tester = app.test_client(self)
+        app.config['TESTING'] = True
+
+        res = tester.get('/users/test_user')
+        data = res.data.decode('utf8').replace("'", '"')
+        data = json.loads(data)
+
+        expected_output = {"test_user": {"id": "test","name": "Test User","favorite_color": "Black"}}
+        self.assertEqual(data, expected_output)
+
+    def test_third(self):
+        tester = app.test_client(self)
+        app.config['TESTING'] = True
+
+        res = tester.get('/users/user1234')
+        data = res.data.decode('utf8').replace("'", '"')
+
+        expected_output = 'user does not exist in the database'
+        self.assertEqual(data, expected_output)
+        self.assertEqual(res.status_code, 404)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
